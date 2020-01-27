9991607be6e49a0fd098bf96175eb4d2799a8538
Utsav-Minal <60209926+Utsav-Minal@users.noreply.github.com>
2020-01-27 11:14:41 +0530

    Update expcon.py

--- expcon.py
+++ expcon.py
@@ -7,11 +7,17 @@ Created on Wed Jan 22 17:22:42 2020
 
 from kafka import KafkaConsumer
 import sys
+
 bootstrap_servers = ['localhost:9092']
+
 topicName = 'myTopic'
+
 consumer = KafkaConsumer (topicName, group_id = 'group1',bootstrap_servers = bootstrap_servers,auto_offset_reset = 'earliest')
+
 try:
     for message in consumer:
         print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,message.offset, message.key,message.value))
+        
+        
 except KeyboardInterrupt:
     sys.exit()

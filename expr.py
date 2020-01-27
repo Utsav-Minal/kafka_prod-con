16b408f1ab5575aa0a4f06fefef61db8656d56a4
Utsav-Minal <60209926+Utsav-Minal@users.noreply.github.com>
2020-01-27 11:12:09 +0530

    Update expr.py

--- expr.py
+++ expr.py
@@ -5,11 +5,29 @@ Created on Wed Jan 22 16:14:19 2020
 @author: Utsav.minal
 """
 from kafka import KafkaProducer
+import schedule 
+import time
+
+
 bootstrap_servers = ['localhost:9092']
 topicName = 'myTopic'
 producer = KafkaProducer(bootstrap_servers = bootstrap_servers)
+
 producer = KafkaProducer()
-ack = producer.send(topicName, b'Hello World!!!!!!!!')
-metadata = ack.get()
-print(metadata.topic)
-print(metadata.partition)
+
+
+def real():
+   
+    ack = producer.send(topicName,b'new line 12')
+    metadata = ack.get()
+    print(metadata.topic)
+    print(metadata.partition)
+    producer.flush()
+    
+schedule.every(3).seconds.do(real)
+
+i=3
+while i>0:
+    schedule.run_pending()
+    time.sleep(3)
+    i=i-1

e1299439940642ac04b175f602690088a0bf29cd
Utsav-Minal <60209926+Utsav-Minal@users.noreply.github.com>
2020-02-15 23:37:51 +0530

    Update expcon.py

--- expcon.py
+++ expcon.py
@@ -1,22 +1,22 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 22 17:22:42 2020
-
-@author: Utsav.minal
-"""
-
 from kafka import KafkaConsumer
+import msgpack
 import sys
+
+
 
 bootstrap_servers = ['localhost:9092']
 
-topicName = 'myTopic'
+topicName = 'kafka_my_Topic'
 
-consumer = KafkaConsumer (topicName, group_id = 'group1',bootstrap_servers = bootstrap_servers,auto_offset_reset = 'earliest')
+#,auto_offset_reset = 'latest'
+consumer = KafkaConsumer (topicName, group_id = 'group1',bootstrap_servers = bootstrap_servers,auto_offset_reset = 'latest')
 
 try:
     for message in consumer:
+        KafkaConsumer(value_deserializer=msgpack.unpackb)
         print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,message.offset, message.key,message.value))
+        
+        #print (KafkaConsumer(value_deserializer=lambda m: json.loads(m.decode('ascii'))))
         
         
 except KeyboardInterrupt:
